d91c912eb93d4ba5a452a9089d5dbac9690a0b58
Umarani <umaranimekala692@gmail.com>
2026-04-23 11:56:20 +0530

    final deployment

--- combine_model.py
+++ combine_model.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import LabelEncoder  # ✅ ADD
+from sklearn.preprocessing import LabelEncoder  #  ADD
 
 # Load dataset
 df = pd.read_csv("data/symptom_based_vitamin_deficiency_dataset_final.csv")
@@ -12,33 +12,33 @@ df.columns = df.columns.str.replace(" ", "_")
 print(df.columns)   
 
 target_col = "Predicted_Deficiency"
-# ✅ FILTER DATA
+#  FILTER DATA
 df = df[df[target_col] != "No Deficiency"]
 
 
-# 🔥 ADD THIS HERE (BALANCING)
+#  ADD THIS HERE (BALANCING)
 from sklearn.utils import resample
 
 df = df.groupby(target_col).apply(
     lambda x: resample(x, replace=True, n_samples=100, random_state=42)
 ).reset_index()
-df = df.drop(columns=["level_1"], errors="ignore")   # ✅ ADD HERE
+df = df.drop(columns=["level_1"], errors="ignore")   # ADD HERE
 
 
- # ✅ ADD THIS
+ #  ADD THIS
 print("COLUMNS AFTER GROUP:", df.columns)
 
 print(df[target_col].value_counts())
 # Split
 X = df.drop(target_col, axis=1)
 
-# ✅ FIX STARTS HERE
+#  FIX STARTS HERE
 le = LabelEncoder()
 y = le.fit_transform(df[target_col])
 
 # save label encoder
 pickle.dump(le, open("label_encoder.pkl", "wb"))
-# ✅ FIX ENDS HERE
+#  FIX ENDS HERE
 
 # One-hot encoding
 X = pd.get_dummies(X)
